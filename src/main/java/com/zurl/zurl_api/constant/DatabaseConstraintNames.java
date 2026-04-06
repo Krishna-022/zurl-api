ca763e1ff1c4a29fd8181389d9f12e1979c43a9c
Krishna <krishnagupta2322@gmail.com>
2026-04-06 10:44:04 +0530

    Update controller response from plain text/string to JSON

--- src/main/java/com/zurl/zurl_api/constant/DatabaseConstraintNames.java
+++ src/main/java/com/zurl/zurl_api/constant/DatabaseConstraintNames.java
@@ -2,7 +2,7 @@ package com.zurl.zurl_api.constant;
 
 public interface DatabaseConstraintNames {
 	
-	static String UNIQUE_ENCODED_KEY = "uniquezurlencodedkey";
+	String UNIQUE_ENCODED_KEY = "uniquezurlencodedkey";
 	
-	static String UNIQUE_LONG_URL = "uniquezurlongurl";
+	String UNIQUE_LONG_URL = "uniquezurlongurl";
 }
--- src/main/java/com/zurl/zurl_api/controller/UrlMapController.java
+++ src/main/java/com/zurl/zurl_api/controller/UrlMapController.java
@@ -1,15 +1,13 @@
 package com.zurl.zurl_api.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.zurl.zurl_api.constant.ApiPathConstants;
 import com.zurl.zurl_api.dto.in.LongUrlInDto;
+import com.zurl.zurl_api.dto.out.ShortUrlOutDto;
 import com.zurl.zurl_api.serviceImpl.UrlMapService;
 import com.zurl.zurl_api.validator.UrlValidator;
-
 import jakarta.validation.Valid;
 
 @RestController
@@ -18,10 +16,10 @@ public class UrlMapController {
 	private final UrlMapService urlMapService;
 	
 	@PostMapping(ApiPathConstants.URL)
-	public ResponseEntity<String> shortTheUrl(@RequestBody @Valid LongUrlInDto longUrlInDto) {
+	public ShortUrlOutDto shortTheUrl(@RequestBody @Valid LongUrlInDto longUrlInDto) {
 		UrlValidator.validateUrl(longUrlInDto.getLongUrl());
 		String shortUrl = urlMapService.shortTheUrl(longUrlInDto.getLongUrl());
-		return ResponseEntity.status(HttpStatus.CREATED).body(shortUrl);
+		return new ShortUrlOutDto(shortUrl);
 	}
 
 	public UrlMapController(UrlMapService urlMapService) {
